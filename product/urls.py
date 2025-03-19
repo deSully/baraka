fc3fb5e8523e2e3588852fc44ca2dc8ae5d698ec
Williams de SOUZA <williams.stanley.desouza@gmail.com>
2025-03-20 00:58:33 +0100

    feat :  add models for categories

--- product/urls.py
+++ product/urls.py
@@ -4,7 +4,7 @@ from .views import ProductViewSet, CategoryListView
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet)  # API will be available at /api/products/
-router.register(r'categories', CategoryListView)  # API will be available at /api/products/
+router.register(r'categories', CategoryListView, basename='categories')  # API will be available at /api/categories/
 
 urlpatterns = [
     path('', include(router.urls)),
