5d297c5f0e8edb477d8cc7c71e71d9b59907f0b8
Williams de SOUZA <williams.stanley.desouza@gmail.com>
2025-03-23 22:14:36 +0100

    feat : admin for merchant

--- merchant/admin.py
+++ merchant/admin.py
@@ -1,28 +1,7 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from .models import Merchant
 
-
 @admin.register(Merchant)
-class MerchantAdmin(UserAdmin):
-    list_display = ("phone_number", "device_id", "first_name", "last_name", "is_active", "created_at")
-    list_filter = ("is_active", "created_at")
-    search_fields = ("phone_number", "device_id", "first_name", "last_name")
-    ordering = ("-created_at",)
-    
-    fieldsets = (
-        (None, {"fields": ("phone_number", "device_id", "activation_code")}),
-        ("Personal Info", {"fields": ("first_name", "last_name")}),
-        ("Permissions", {"fields": ("is_active",)}),
-        ("Important Dates", {"fields": ("created_at",)}),
-    )
-
-    add_fieldsets = (
-        (None, {
-            "classes": ("wide",),
-            "fields": ("phone_number", "device_id", "activation_code", "is_active"),
-        }),
-    )
-
-    readonly_fields = ("created_at",)
-
+class MerchantAdmin(admin.ModelAdmin):
+    list_display = ("phone_number", "device_id", "is_active", "created_at")
+    search_fields = ("phone_number", "device_id")
