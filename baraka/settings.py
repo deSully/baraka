b6ad9057ab3e2a6f68f99de2b30657c0f54e9b00
Williams de SOUZA <williams.stanley.desouza@gmail.com>
2025-05-05 08:24:07 +0000

    setup ssl

--- baraka/settings.py
+++ baraka/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-z^ozw%rwp-mgdbt2mp0!)w4wyf_&f-w@4dhusek5h9+-cv-8k*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
