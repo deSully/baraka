700b925f85b6c02a17441db60c284501d09a263b
Williams de SOUZA <williams.stanley.desouza@gmail.com>
2025-05-05 08:09:04 +0000

    setup ssl

--- baraka/settings.py
+++ baraka/settings.py
@@ -126,7 +126,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+# URL à utiliser pour servir les fichiers statiques
+STATIC_URL = '/static/'
+
+# Dossier où Django collectera tous les fichiers statiques avec collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# URL publique des fichiers médias (upload utilisateur)
+MEDIA_URL = '/media/'
+
+# Dossier où les fichiers médias seront stockés
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
